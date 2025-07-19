7a6edc461a39b120c962af5d35af5af913188b39
EvanMaa <evanmayx7@gmail.com>
2025-07-19 14:10:21 -0400

    Update main.py

--- server/main.py
+++ server/main.py
@@ -7,10 +7,7 @@ import subprocess
 import json
 import os
 from dotenv import load_dotenv
-<<<<<<< Updated upstream
-=======
 from pymongo import MongoClient
->>>>>>> Stashed changes
 
 load_dotenv()
 
